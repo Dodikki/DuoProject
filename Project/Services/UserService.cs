bb682d8ebb0b1d2c46625411ea2716ea4b3b3cea
student <student@DESKTOP-01CVNQN>
2026-01-31 13:54:44 +0300

    Добавьте файлы проекта.

---
+++ Project/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Project.Dto;
+using Project.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Services
+{
+    internal class UserService
+    {
+        private readonly HttpClient _http;
+
+        public UserService()
+        {
+            _http = ApiClient.Instance;
+        }
+
+        public async Task<List<UserDto>> GetAllAsync()
+        {
+            try
+            {
+                return await _http.GetFromJsonAsync<List<UserDto>>("/api/users");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error HTTP request: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error get all users: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
