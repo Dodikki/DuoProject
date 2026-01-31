bf1256557ff58ba7e4515103089749778734bc9d
Dodikki <slipslip727296@gmail.com>
2026-01-31 14:48:32 +0300

    In PostDto add Id

--- Project/Dto/PostDto.cs
+++ Project/Dto/PostDto.cs
@@ -8,7 +8,10 @@ namespace Project.Dto
 {
     internal class PostDto
     {
+        public int Id { get; set; } 
+
         public string Title { get; set; }
+
         public string Content { get; set; }
         
     }
